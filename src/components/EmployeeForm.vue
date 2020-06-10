bc0a692bec0baf233917c0bd765d1339f4a282ef
Sybille Kasslatter <sybille.kasslatter@hotmail.com>
2020-06-10 18:10:43 +0200

    Created form

---
+++ src/components/EmployeeForm.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="employee-form">
+    <form @submit.prevent="handleSubmit">
+      <label>Employee name</label>
+      <input v-model="employee.name" type="text" />
+      <label>Employee Email</label>
+      <input v-model="employee.email" type="text" />
+      <button>Add Employee</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "employee-form",
+  data() {
+    return {
+      submitting: false,
+      error: false,
+      success: false,
+      employee: {
+        name: "",
+        email: ""
+      }
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.submitting = true;
+      this.clearStatus();
+
+      if (this.invalidName || this.invalidEmail) {
+        this.error = true;
+        return;
+      }
+
+      this.$emit("add:employee", this.employee);
+      this.employee = {
+        name: "",
+        email: ""
+      };
+      this.error = false;
+      this.success = true;
+      this.submitting = false;
+    },
+    clearStatus() {
+      this.success = false;
+      this.error = false;
+    },
+    computed: {
+      invalidName() {
+        return this.employee.name === "";
+      },
+
+      invalidEmail() {
+        return this.employee.email === "";
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+form {
+  margin-bottom: 2rem;
+  text-align: center;
+}
+button {
+  margin-top: 1rem;
+}
+</style>
---
+++ src/components/EmployeeTable.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="employee-table">
+    <table>
+      <thead>
+        <tr>
+          <th>Employee name</th>
+          <th>Employee email</th>
+          <th>ID</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="employee in employees" :key="employee.id">
+          <td>{{ employee.name }}</td>
+          <td>{{ employee.email }}</td>
+          <td>{{ employee.id }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "employee-table",
+  props: {
+    employees: Array
+  }
+};
+</script>
+
+<style scoped></style>
